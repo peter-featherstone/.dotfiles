44148a51317052e36ccfbb4dff656b066776df24
Peter Featherstone <fe@therst.one>
2024-11-18 16:07:22 +0000

    Add keys to launch the telescope functions I want

--- neovim/init.lua
+++ neovim/init.lua
@@ -77,13 +77,17 @@ require("lazy").setup({
             },
         },
     },
+    -- Telescope allows us to great filesystem searching 
     {
         "nvim-telescope/telescope.nvim", 
-        dependencies = { "nvim-lua/plenary.nvim" }
+        dependencies = { "nvim-lua/plenary.nvim" },
+        config = function()
+            local builtin = require 'telescope.builtin'
+            vim.keymap.set('n', '<leader>ff', builtin.find_files, { desc = '[F]ind [F]iles' })
+            vim.keymap.set('n', '<leader>fg', builtin.live_grep, { desc = '[F]ind by [G]rep' })
+        end
     },
     -- Set tabs and spaces per file type automatically.
     "tpope/vim-sleuth",
-
-
 })
 
