f26787044dba94f2b609edc3a8735b4a6af4546d
Peter Featherstone <fe@therst.one>
2025-03-30 17:35:09 +0100

    Integrate aerospace and sketchybar nicely

--- sketchybar/plugins/aerospace.sh
+++ sketchybar/plugins/aerospace.sh
@@ -4,7 +4,7 @@
 # chmod +x ~/.config/sketchybar/plugins/aerospace.sh
 
 if [ "$1" = "$FOCUSED_WORKSPACE" ]; then
-    sketchybar --set $NAME background.drawing=on
+    sketchybar --set $NAME background.drawing=on label.color=0xff000000
 else
-    sketchybar --set $NAME background.drawing=off
+    sketchybar --set $NAME background.drawing=off label.color=0xffffffff
 fi
